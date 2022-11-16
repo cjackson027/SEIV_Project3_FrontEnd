f7ead40278ff28c376094c7fabe997efb1ad7314
JuliaFlores21 <111401638+JuliaFlores21@users.noreply.github.com>
2022-11-16 02:17:16 -0600

    Corrected /2022/project3

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     //host: 'localhost',
     port: 8081
   },
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/project3/2022/t4/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/2022/project3/t4/',
   transpileDependencies: ['vuetify'],
 }
 

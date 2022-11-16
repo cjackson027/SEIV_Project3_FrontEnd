0063c34657fbfb88abf43aaec16665b1a20824b4
JuliaFlores21 <julia.flores@eagles.oc.edu>
2022-11-15 19:54:21 -0600

    Add and Edit Office Hours Page

--- src/views/EditOfficeHours.vue
+++ src/views/EditOfficeHours.vue
@@ -9,41 +9,41 @@
         <br>
         <h4>{{ message }}</h4>
         <br>
-        <h4>Office Hour: {{officelId}}</h4>
+        <h4>User: {{userId}} Semester: {{semesterId}} Office Hours: {{officeId}}</h4>
         <br>
         <v-form 
           ref="form" 
           v-model="valid"
           lazy validation
         >
-          <v-text-field
+        <v-text-field
             v-model="office.officeWeekDay"
             id="officeWeekDay"
             :counter="50"
             label="WeekDay"
             required
-          ></v-text-field>
-          <v-text-field
+        ></v-text-field>
+        <v-text-field
             v-model="office.officeStartTime"
             id="officeStartTime"
             :counter="50"
             label="Start Time"
             required
-          ></v-text-field>
+        ></v-text-field>
 
-          <v-text-field
-             v-model="office.officeEndTime"
-             id="officeEndTime"
-             :counter="50"
-             label="End Time"
-             required
-          ></v-text-field>
+        <v-text-field
+            v-model="office.officeEndTime"
+            id="officeEndTime"
+            :counter="50"
+            label="End Time"
+            required
+        ></v-text-field>
   
           <v-btn
             :disabled="!valid"
             color="success"
             class="mr-4"
-            @click="saveOfficeHour()"
+            @click="saveOfficeHours()"
           >
             Save
           </v-btn>
@@ -62,22 +62,29 @@
   
   <script>
   import OfficeServices from "../services/officeServices";
+  
   export default {
-    name: "add-office",
-    props: ['userId', 'semesterId'],
+    name: "edit-office",
+    props: [ 'userId' , 'semesterId', 'officeId' ],
     data() {
       return {
-        valid: true,
-        office: {
-          officeWeekDay: "",
-          officeStartTime: "",
-          officeEndTime: ""
-        },
+        valid: false,
+        office: {},
         message: "Enter data and click save"
       };
     },
     methods: {
-      saveOfficeHour() {
+      retrieveOfficeHours() {
+        OfficeServices.getOfficeHours(this.userId, this.semesterId, this.officeId)
+          .then(response => {
+            this.office= response.data;
+          })
+          .catch(e => {
+            this.message = e.response.data.message;
+          });
+  
+      },
+      saveOfficeHours() {
         var data = {
           weekDay: this.office.officeWeekDay,
           startTime: this.office.officeStartTime,
@@ -85,19 +92,22 @@
           userId : this.userId,
           semesterId : this.semesterId
         };
-        OfficeServices.createOffice(this.userId, this.semesterId, data)
+        OfficeServices.updateOfficeHours(this.office.userId, this.office.semesterId, this.office.id, data)
           .then(response => {
-            this.office.officeId = response.data.id;
+            this.office.id = response.data.id;
           
-            this.$router.push({ name: 'view' , params: { userId: this.userId, semesterId: this.semesterId }} );
+           this.$router.push({ name: 'view' , params: { userId: this.office.userId, semesterId: this.office.semesterId }} );
           })
           .catch(e => {
             this.message = e.response.data.message;
           });
       },
       cancel(){
-          this.$router.push({ name: 'view' , params: { userId: this.userId, semesterId: this.semesterId }} );
+          this.$router.push({ name: 'view' , params: { userId: this.office.userId, semesterId: this.office.semesterId }} );
       }
+    },
+      mounted() {
+        this.retrieveOfficeHours();
     }
   }
   

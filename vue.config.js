65249fb7ec80fbd44556de67dd2b2f24e01e4e29
cjackson027 <78748096+cjackson027@users.noreply.github.com>
2022-11-16 01:34:21 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    host: 'localhost',
+    //host: 'localhost',
     port: 8081
   },
   publicPath: process.env.NODE_ENV === 'development' ? '/' : '/project3/2022/t4/',
